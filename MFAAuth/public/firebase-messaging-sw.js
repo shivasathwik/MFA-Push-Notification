c42c86531540098795ade2c150e35c6c2a918f48
ShivaSathwik B R <39687924+shivasathwik@users.noreply.github.com>
2021-12-11 21:53:10 -0600

    Update firebase-messaging-sw.js

--- MFAAuth/public/firebase-messaging-sw.js
+++ MFAAuth/public/firebase-messaging-sw.js
@@ -9,7 +9,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.13.0/firebase-messaging.js')
 Initialize the Firebase app in the service worker by passing in the messagingSenderId.
 */
 firebase.initializeApp({
-    'messagingSenderId': '521731395720'
+    'messagingSenderId': //FCM Sender ID
 })
 
 /*
